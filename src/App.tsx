6fd356b75951fc1ba7f6b6cfa0fa902aa85ff119
dwright6633 <103792319+dwright6633@users.noreply.github.com>
2024-03-06 13:26:43 -0700

    NCAA basketball practice

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,77 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import teamData from './CollegeBasketballTeams.json';
+
+interface TeamProps {
+  tid: number;
+  cid: number;
+  did: number;
+  school: string;
+  name: string;
+  abbrev: string;
+  pop: number;
+  city: string;
+  state: string;
+  latitude: number;
+  longitude: number;
+}
+
+function Welcome() {
+  return <h1>Welcome to the NCAA Teams List</h1>;
+}
+
+// Old version:
+// -----------------------------------------------------
+class Team extends React.Component<TeamProps> {
+  render() {
+    const oneTeam = this.props;
+
+    return (
+      <div>
+        <img src="" alt="" />
+        <h2>{oneTeam.school}</h2>
+        <h3>{oneTeam.name}</h3>
+        <h3>
+          {oneTeam.city}, {oneTeam.state}
+        </h3>
+      </div>
+    );
+  }
+}
+
+function TeamList() {
+  return (
+    <div>
+      {teamData.teams.map((teamNum) => (
+        <Team {...teamNum} />
+      ))}
+    </div>
+  );
+}
+
+// // New Version:
+// // -----------------------------------------------------
+// function TeamList() {
+//   return (
+//     <div>
+//       {teamData.teams.map((teamNum) => (
+//         <div className="border-box" key={teamNum.tid}>
+//           <h2>{teamNum.school}</h2>
+//           <p>{teamNum.name}</p>
+//           <p>
+//             {teamNum.city}, {teamNum.state}
+//           </p>
+//         </div>
+//       ))}
+//     </div>
+//   );
+// }
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Welcome />
+      <TeamList />
     </div>
   );
 }
